6e47a9c7865d89b18c485dce53bc0fd9a51a403c
谢福泰 <17600636164@163.com>
2019-08-18 03:47:00 +0800

    修改返回按钮

--- lib/widget/webview.dart
+++ lib/widget/webview.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 //webview黑名单 这些地址不能跳转 如果跳转到这些地址  直接返回app
-const CATCH_URLS = ['m.ctrip.com/','m.ctrip.com/html5','m.ctrip.com/html5/'];
+const CATCH_URLS = ['m.ctrip.com/','m.ctrip.com/html5','m.ctrip.com/html5/','m.ctrip.com/webapp/activity/overseasindex?from=https%3A%2F%2Fm.ctrip.com%2Fhtml5%2F'];
 class WebView extends StatefulWidget {
   final String url; //webview的地址
   final String statusBarColor; //appBar的颜色
@@ -35,6 +35,7 @@ class _WebViewState extends State<WebView> {
     _onStateChanged = webviewReference.onStateChanged.listen((WebViewStateChanged state) {
       switch(state.type) {
         case WebViewState.startLoad:
+          print('state.urlstate.urlstate.urlstate.url${state.url}');
           //如果当前的url包含在定义的CATCH_URLS里面  并且不能让程序跳转到这个url里面
           if(_isToMain(state.url) && !exiting) {
             //如果禁止返回 就还加载当前h5页面
@@ -71,20 +72,20 @@ class _WebViewState extends State<WebView> {
   @override
   void dispose() {
     // TODO: implement dispose
-    super.dispose();
     //取消监听
     _onUrlChange.cancel();
     _onStateChanged.cancel();
     _onHttpError.cancel();
     webviewReference.dispose();
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
     String statusBarColorStr = widget.statusBarColor ?? 'ffffff';
     Color backButtonColor;
-    if(statusBarColorStr == 'ffffff') {
+    if (statusBarColorStr == 'ffffff') {
       backButtonColor = Colors.black;
-    }else {
+    } else {
       backButtonColor = Colors.white;
     }
     return Scaffold(
@@ -113,9 +114,8 @@ class _WebViewState extends State<WebView> {
 
   //打开webview的appBar的显示方式
   _appBar(Color backGroundColor, Color backButtonColor) {
-    print('widget.hideAppBar'+widget.hideAppBar.toString());
     //第一种显示
-    if(widget.hideAppBar) {
+    if(widget.hideAppBar ?? false) {
       return Container(
         color: backGroundColor,
         height: 30,
@@ -123,6 +123,8 @@ class _WebViewState extends State<WebView> {
     }
     //第二种方式显示
     return Container(
+      color: backButtonColor,
+      padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
       child: FractionallySizedBox(
         widthFactor: 1,
         child: Stack(
@@ -130,8 +132,11 @@ class _WebViewState extends State<WebView> {
             GestureDetector(
               child: Container(
                 margin: EdgeInsets.only(left: 10),
-                child: Icon(Icons.close,color: backButtonColor,size: 26,),
+                child: Icon(Icons.close,color: widget.statusBarColor == '4289ff' ?  Colors.black :  backButtonColor,size: 26,),
               ),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
             Positioned(
               left: 0,
