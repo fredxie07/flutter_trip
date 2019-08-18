7d80a14e54ec11e88b3089b220210a7a21e80d5a
谢福泰 <17600636164@163.com>
2019-08-18 16:52:01 +0800

    修改跳转后appBar颜色

--- lib/widget/webview.dart
+++ lib/widget/webview.dart
@@ -5,80 +5,83 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 //webview黑名单 这些地址不能跳转 如果跳转到这些地址  直接返回app
 const CATCH_URLS = ['m.ctrip.com/','m.ctrip.com/html5','m.ctrip.com/html5/','m.ctrip.com/webapp/activity/overseasindex?from=https%3A%2F%2Fm.ctrip.com%2Fhtml5%2F'];
 class WebView extends StatefulWidget {
-  final String url; //webview的地址
-  final String statusBarColor; //appBar的颜色
-  final String title; //appBar的title
-  final bool hideAppBar; //是否隐藏appBar
-  final bool backForbid; //是否禁止返回app
+  String url;
+  final String statusBarColor;
+  final String title;
+  final bool hideAppBar;
+  final bool backForbid;
 
-  const WebView({Key key, this.url, this.statusBarColor, this.title, this.hideAppBar, this.backForbid = false}) : super(key: key);
+  WebView(
+      {this.url,
+        this.statusBarColor,
+        this.title,
+        this.hideAppBar,
+        this.backForbid = false}) {
+    if (url != null && url.contains('ctrip.com')) {
+      //fix 携程H5 http://无法打开问题
+      url = url.replaceAll("http://", 'https://');
+    }
+  }
 
   @override
   _WebViewState createState() => _WebViewState();
 }
 
 class _WebViewState extends State<WebView> {
-  final webviewReference = new FlutterWebviewPlugin();
-  StreamSubscription<String> _onUrlChange;
+  final webviewReference = FlutterWebviewPlugin();
+  StreamSubscription<String> _onUrlChanged;
   StreamSubscription<WebViewStateChanged> _onStateChanged;
   StreamSubscription<WebViewHttpError> _onHttpError;
-  bool exiting = false; //是否从webview返回过app
+  bool exiting = false;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    //监听页面url变化  返回值为stream
-    _onUrlChange = webviewReference.onUrlChanged.listen((String url) {
-
-    });
-    //H5页面发生变化时的监听 （用户切换页面时）返回stream
-    _onStateChanged = webviewReference.onStateChanged.listen((WebViewStateChanged state) {
-      switch(state.type) {
-        case WebViewState.startLoad:
-          print('state.urlstate.urlstate.urlstate.url${state.url}');
-          //如果当前的url包含在定义的CATCH_URLS里面  并且不能让程序跳转到这个url里面
-          if(_isToMain(state.url) && !exiting) {
-            //如果禁止返回 就还加载当前h5页面
-            if(widget.backForbid) {
-              webviewReference.goBack();
-            }else {
-              Navigator.pop(context);
-              exiting = true;
-            }
-          }
-          break;
-        default:
-          break;
-      }
-    });
-    //打开webview发生网络错误
-    _onHttpError = webviewReference.onHttpError.listen((WebViewHttpError error) {
-        print(error.toString());
-    });
-    //防止页面重复打开
     webviewReference.close();
+    _onUrlChanged = webviewReference.onUrlChanged.listen((String url) {});
+    _onStateChanged =
+        webviewReference.onStateChanged.listen((WebViewStateChanged state) {
+          switch (state.type) {
+            case WebViewState.startLoad:
+              if (_isToMain(state.url) && !exiting) {
+                if (widget.backForbid) {
+                  webviewReference.launch(widget.url);
+                } else {
+                  Navigator.pop(context);
+                  exiting = true;
+                }
+              }
+              break;
+            default:
+              break;
+          }
+        });
+    _onHttpError =
+        webviewReference.onHttpError.listen((WebViewHttpError error) {
+          print(error);
+        });
   }
 
   _isToMain(String url) {
     bool contain = false;
-    for(final  value in CATCH_URLS) {
-      if(url ?. endsWith(value) ?? false) {
+    for (final value in CATCH_URLS) {
+      if (url?.endsWith(value) ?? false) {
         contain = true;
         break;
       }
     }
     return contain;
   }
+
   @override
   void dispose() {
-    // TODO: implement dispose
-    //取消监听
-    _onUrlChange.cancel();
     _onStateChanged.cancel();
+    _onUrlChanged.cancel();
     _onHttpError.cancel();
     webviewReference.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     String statusBarColorStr = widget.statusBarColor ?? 'ffffff';
@@ -91,10 +94,11 @@ class _WebViewState extends State<WebView> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          _appBar(Color(int.parse('0xff'+statusBarColorStr)),backButtonColor),
-//          webview展示的内容
+          _appBar(
+              Color(int.parse('0xff' + statusBarColorStr)), backButtonColor),
           Expanded(
-            child: WebviewScaffold(
+              child: WebviewScaffold(
+                userAgent: 'null',//防止携程H5页面重定向到打开携程APP ctrip://wireless/xxx的网址
                 url: widget.url,
                 withZoom: true,
                 withLocalStorage: true,
@@ -102,47 +106,50 @@ class _WebViewState extends State<WebView> {
                 initialChild: Container(
                   color: Colors.white,
                   child: Center(
-                    child: CircularProgressIndicator(),
+                    child: Text('Waiting...'),
                   ),
                 ),
-            ),
-          )
+              ))
         ],
       ),
     );
   }
 
-  //打开webview的appBar的显示方式
-  _appBar(Color backGroundColor, Color backButtonColor) {
-    //第一种显示
-    if(widget.hideAppBar ?? false) {
+  _appBar(Color backgroundColor, Color backButtonColor) {
+    if (widget.hideAppBar ?? false) {
       return Container(
-        color: backGroundColor,
+        color: backgroundColor,
         height: 30,
       );
     }
-    //第二种方式显示
     return Container(
-      color: backButtonColor,
+      color: backgroundColor,
       padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
       child: FractionallySizedBox(
         widthFactor: 1,
         child: Stack(
           children: <Widget>[
             GestureDetector(
-              child: Container(
-                margin: EdgeInsets.only(left: 10),
-                child: Icon(Icons.close,color: widget.statusBarColor == '4289ff' ?  Colors.black :  backButtonColor,size: 26,),
-              ),
               onTap: () {
                 Navigator.pop(context);
               },
+              child: Container(
+                margin: EdgeInsets.only(left: 10),
+                child: Icon(
+                  Icons.close,
+                  color: backButtonColor,
+                  size: 26,
+                ),
+              ),
             ),
             Positioned(
               left: 0,
               right: 0,
               child: Center(
-                child: Text(widget.title ?? '',style: TextStyle(color: backButtonColor,fontSize: 20),),
+                child: Text(
+                  widget.title ?? '',
+                  style: TextStyle(color: backButtonColor, fontSize: 20),
+                ),
               ),
             )
           ],
@@ -151,3 +158,4 @@ class _WebViewState extends State<WebView> {
     );
   }
 }
+
