342e35eb811082c39582902a9e67290dadbe6bfb
谢福泰 <17600636164@163.com>
2019-08-18 22:51:51 +0800

    搜索页输入框

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trips/dao/search_dao.dart';
+import 'package:flutter_trips/model/search_model.dart';
+import 'package:flutter_trips/navigator/tab_navigator.dart';
+import 'package:flutter_trips/widget/search_bar.dart';
 
+const URL = 'https://m.ctrip.com/restapi/h5api/searchapp/search?source=mobileweb&action=autocomplete&contentType=json&keyword=';
 class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -9,10 +14,24 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('搜索页'),
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          SearchBar(
+            hideLeft: false,
+            defaultText: '哈哈',
+            hint: '123',
+            leftButtonClick: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => TabNavigator()));
+            },
+            onChange: _onTextChanged,
+          ),
+        ],
       )
     );
+  }
+  _onTextChanged(text) {
+    print(text);
   }
 }
 
